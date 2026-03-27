0ff5686d2f8f718c01bbe4b90e23e323950c16d5
coderhabiba <anikahabiba50@gmail.com>
2026-03-27 13:05:48 +0600

    tailwind and daisy setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import ChatbotIcon from './components/ChatbotIcon';
 
 const App = () => {
   return (
     <div className='container'>
       <div className='chat-header'>
         <div className="header-info">
+          <ChatbotIcon/>
           <h2 className="logo-text">
             Chatbot
           </h2>
